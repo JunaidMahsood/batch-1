d5b93fdf387afb356102b99da463c98f42ae449a
JunaidMahsood <ruqaiya572@gmail.com>
2025-07-07 16:01:54 +0500

    Update quizData.js

--- quizData.js
+++ quizData.js
@@ -5,6 +5,740 @@
 
 
 
+[TIMER: 70]
+
+
+1.solar system was discovered by?
+
+A. Aristotle
+
+*B. Nicolaus Copernicus
+
+C. Claudius Ptolemy
+
+D. Galileo Galilei
+
+2.Which of the following planets also has no moon?
+
+A. Jupiter
+
+B. Uranus
+
+C. Saturn
+*D. Venus
+
+
+3.Who was the first person to observe Saturn through a telescope?
+
+A. Nicolaus Copernicus
+
+*B. Galileo Galilei
+
+C. Johannes Kepler
+
+D. Isaac Newton
+
+4.What is the main cause of an earthquake?
+A. Heavy rainfall
+
+*B. Sudden movement of tectonic plates
+
+C. High temperature
+
+D. Ocean tides
+
+
+
+5.What does the third law of thermodynamics state?
+A. Energy cannot be created or destroyed
+
+B. Entropy of the universe always increases
+
+C. Heat flows from hot to cold body
+
+*D. Entropy of a perfect crystal becomes zero at absolute zero temperature
+
+
+
+6.What type of lens is used to correct long-sightedness?
+A.diverging lens
+B.concave lens 
+*C.Convex lens 
+D.None of these
+
+7.Who gave the first scientific theory of evolution?
+
+*A. Jean-Baptiste Lamarck
+
+B. Charles Darwin
+
+C. Gregor Mendel
+
+D. Aristotle
+
+
+
+8.What is the chemical name of laughing gas?
+
+A. Carbon dioxide
+
+B. Ammonia
+
+*C. Nitrous oxide
+
+D. Sulphur dioxide
+
+9.Which layer of the atmosphere is closest to the Earth's surface?
+A.Troposphere
+B.Stratosphere
+C.Exosphere
+*D.None of these
+
+10.Absolute zero temperature is taken as:
+*A.- 273.15°C 
+B.273°C 
+C.- 373.15°C
+D.0°C
+
+11.What is chromatography?
+
+A. A method to boil liquids
+
+*B. A technique to separate mixtures
+C. A way to freeze substances
+D. A method to burn chemicals
+
+12.Deficiency of vitamin B1 (thiamine) causes which disease?
+
+A. Scurvy
+
+B. Rickets
+
+*C. Beriberi
+
+D. Pellagra
+
+13.Enzymes speed up:
+
+A. Blood flow
+
+B. Muscle movement
+
+*C. Chemical reactions
+
+D. Cell division only
+
+14.What does "CT" in CT scan stand for?
+
+*A. Computed Tomography
+B. Cell Test
+
+C. Clinical Test
+
+D. Central Transmission
+
+
+
+15.What is the main use of a fathometer?
+
+A. Measuring air pressure
+
+B. Detecting earthquakes
+
+C. Measuring temperature
+
+
+*D.Measuring depth of water bodies
+
+
+16.What is lalophobia?
+
+A. Fear of heights
+
+B. Fear of water
+
+*C. Fear of speaking
+
+D. Fear of writing
+
+
+
+17.Who is credited with inventing the first thermometer?
+
+A. Albert Einstein
+
+B. Alexander Fleming
+
+*C. Galileo Galilei
+
+D. Isaac Newton
+
+18.Which of the following is an example of an exocrine gland?
+
+A. Pituitary gland
+
+B. 
+Thyroid gland
+
+
+C. Adrenal gland
+
+*D. Salivary gland 
+
+19.What are beta rays?
+
+*A.Streams of high-energy electrons or positrons
+B. Electromagnetic waves
+ 
+C. Neutrons
+
+D. Protons
+
+20.In which direction do lines of latitude run?
+
+A. North to South
+
+B. Diagonally
+
+*C. East to West
+
+D. Vertically
+
+
+
+21.According to which constitution, Pakistan became an Islamic Republic?
+*A.1956 
+B.1962
+C.1973 
+D.None of these
+
+22.Which agreement was signed between India and Pakistan in 1966 to resolve the conflict from the 1965 war?
+A.Lahore Declaration
+*B.Tashkent Agreement 
+C.Shimla Agreement
+D.Agra Summit
+
+23.According to the 1973 constitution of Pakistan, the minimum age to become Prime Minister is: 
+*A.25 years  
+B.35 years 
+C.45 years  
+D.55 years 
+
+24.n which year did Pakistan purchase Gwadar from Oman?
+A.1956
+*B.1958 
+C.1962
+D.None of these
+
+25.The office of Governor-General of Pakistan was replaced by the office of President on:
+A.14 August 1947 
+B.16 October 1951
+*C.23 March 1956 
+D.05 July 1955
+
+26.The first general elections in Pakistan were held on:
+A.Dec 5, 1970 
+B.Dec 6, 1970
+*C.Dec 7, 1970 
+D.Dec 8, 1970
+
+27.The first space satellite launched by Pakistan was:
+*A.Rehbar-1 
+B.Shaheen-1
+C.Badr-II
+D.None of these
+
+28.The Shimla Agreement between Pakistan and India was signed on:
+A.2 July 1960
+B.2 July 1965
+C.2 July 1970 
+*D.2 July 1972 
+
+29.After Indus river, the longest river in Pakistan is:
+*A.Sutlej River
+B.Ravi River
+C.Chinab river
+D.None of these
+ 
+30.The fourth Prime Minister of Pakistan was:
+A.I.I Chundrigar 
+*B.Chaudhry Muhammad Ali 
+C.Huseyn Shaheed Suhrawardy
+D.None of these
+
+31.The First census in Pakistan was held in:
+A.1947
+*B.1951 
+C.1961
+D.1982
+
+32."The Emergence of Pakistan" book is written by: 
+A.Shuja Nawaz 
+*B.Ch. Muhammad Ali 
+C.Ayub Khan
+D.None of these
+
+33.Military courts in Pakistan were established under which amendment? 
+A.20th
+*B.21st 
+C.22nd 
+D.23rd
+  
+34.The state of swat Integrated into Pakistan in:
+A.1968
+*B.1969 
+C.1970
+D.None of these
+
+35.The first recipient of Nishan-e-Haider in Pakistan was:
+A.Tufail Muhammad 
+B.Aziz Bhatti
+*C.Raja Muhammad Sarwar 
+D.Lalak Jan
+
+36.In which year was the Khilafat Movement launched in the Indian subcontinent?
+*A.1919 
+B.1920 
+C.1922
+D.1925
+
+37.How many peaks above 8,000 meters are found in Pakistan?
+A.3
+*B.5 
+C.9
+D.14
+
+38.The famous newspaper “ Comrade” and “Humdard” were launched by:
+*A.Maulana Muhammad Ali Jauhar 
+B.Sir Syed Ahmad Khan
+C.Ch. Rehmat Ali
+D.Shibli Nomani
+
+39.Pak-China border dispute was settled in:
+A.1960
+B.1961
+*C.1963 
+D.None of these 
+
+40.After Sir Agha Khan, the next president of All India Muslim League was:
+A.Wiqar-ul-Mulk
+*B.Raja Sahib of Mahmoodabad 
+C.Mualana Zafer Ali Khan
+D.Mian Muhammad Shafi
+
+41.Hazrat Abu Bakr (RA) belonged to which tribe?
+
+*A.Banu Tamim
+
+B. Banu Umayyah
+
+C. Banu Hashim
+D. Banu Makhzum
+
+42.What does the word Al-Anfal mean?
+
+A. The Helpers
+
+B. The Path
+
+*C. The Spoils of War
+
+D. The Victory
+
+43. How many days were spent in digging the trench during Ghazwa-e-Khandaq?
+
+A. 10 days
+
+B. 15 days
+
+C. 20 days
+*D.6 days
+
+
+44.____________ was called month of migration ?
+A. Muharram
+B. Shaaban
+C. Ramzan
+*D. Rabiul Awal
+
+45.What is a Mutawatir Hadith?
+
+A. A hadith narrated by one companion only
+
+B. A hadith with unknown chain
+
+*C. A hadith narrated by a large number of people in every generation
+
+D. A weak hadith
+
+46.Sunnah Taqririyyah refers to:
+
+A. The actions of the Prophet 
+*B.The approvals (silent consent) of the Prophet
+C.The sayings of the Prophet 
+D. The dreams of the Prophet 
+
+47.What is Qiyas in Islamic law?
+
+*A. Analogical reasoning based on Quran and Sunnah
+B. Consensus of scholars
+
+C. Revelation
+
+D. Personal opinion
+
+48.Who was the youngest son of Prophet Muhammad (SAW)?
+A. Qasim
+
+*B. Ibrahim
+
+C. Abdullah
+
+D. Hasan
+
+49.The second Ashra of Ramadan reflects:
+
+A. Mercy (Rahmah)
+
+B. Punishment
+C. Freedom from Hell
+
+*D. Forgiveness (Maghfirah)
+
+50.Who was the second daughter of Prophet Muhammad(SAW)
+
+A. Fatimah (RA)
+
+*B. Ruqayyah (RA)
+
+C. Umm Kulthum (RA)
+
+D. Zainab (RA 
+
+51.What is the name of the 7th door of Jannah?
+ 
+A. Ar-Rayyan
+
+*B. Al-Iman
+
+C. Al-Sabr
+
+D. Bab Al-Afuw
+
+52. What is Darbe Mehr?
+
+A. A Muslim shrine
+
+B. A Jewish temple
+
+*C. A Zoroastrian fire temple
+
+D. A Buddhist monastery
+
+
+
+53. Ashura is significant because:
+
+A. It marks Eid
+
+*B. It commemorates the martyrdom of Imam Hussain (RA) and many historic events
+
+C. It's the night of Qadr
+
+D. It's the Prophet’s birthday
+
+
+
+54.What was the famous title of Hazrat Khadija (RA)?
+A. Umm al-Mu'mineen
+
+B. Siddiqa
+
+*C. Tahira
+
+D. Amina
+
+
+
+55.The religion associated with Jinja is:
+
+A. Buddhism
+
+B. Confucianism
+
+*C. Shintoism
+
+D. Taoism
+
+56.The grave of the Prophet was prepared by?
+A. Hazrat Abu Bakar
+B. Hazrat Umar
+C. Hazrat Usman
+*D. Hazrat Abu Talha
+
+57.Prophet stayed at Makkah for __________ days after its conquest ?
+A. 5
+B. 10
+*C. 15
+D. 20
+
+58.Which Surah of Quran has Bismillah twice ?
+A. Aal-e-Imraan
+*B. Al-Namal
+C. Yaaseen
+D. Muhammad
+
+59.In surah kahf, which animal is mentioned along with the Ashaab e kahf ?
+*A. Dog
+B. Cat
+C. Horse
+D. woodpecker
+
+60.Umm-ul-Masakeen was the title given to one of the wives of the Prophet (SAW) ?
+A. Harat Sauda (RA)
+B. Hazrat Zainab bente Jehash (RA)
+*C. Hazrat Zainab bente Khuzima (RA)
+D. Hazrat Safia (RA)
+
+61.Always try to be polite __ others.
+*A. to
+B. with
+C. for
+D. on
+
+62.Protractor : Angle :: Ruler : ______ (choose the word that best completes the comparison)
+A. rectangle
+*B. length
+C. classroom
+D. king
+
+63.Antonym of Spurious is _______________?
+A. thoughtless
+B. flexible
+*C. genuine
+D. provident
+
+64.Antonym of SENILE is _____________?
+A. Anile
+B. Doting
+C. Ancient
+*D. Alert
+
+65.Antonym of Kowtow is ___________?
+A. pull
+*B. snub
+C. forage
+D. fawn
+
+66.Synonym of BEMOAN is ___________ ?
+*A. Lament
+B. Denounce
+C. Loathe
+D. Soothe
+
+67.Synonym of cynosure is _________?
+*A. center of attention
+B. obscurity
+C. clown
+D. certainty
+
+68.The “gift of the gab” means you are:
+A.experienced
+B.very intelligent
+*C.skilled in talking
+D.busy
+
+69.“Out of order” means it _____.
+A.is untidy
+*B.doesn’t work
+C.is unavailable
+D.rarel
+
+70.To run away with a lover (one word substitution)
+A.Deceive
+B.Cheat
+C.Escape
+*D.Elope
+
+71.Reproducing or memorizing word for word (one word substitution)
+*A.Verbatim
+B.Verbose
+C.Verbiage
+DVerbalism
+
+72.The plants and vegetation of a region (one word substitution)
+*A.Flora
+B.Fauna
+C.Landscape
+D.Environment
+
+73.The lion has long been a symbol of strength and power but it is a cruel animal.
+A.Complex Sentence
+*B.Compound Sentence
+C.Simple Sentence
+D.Compound-Complex Sentence
+
+74.While the police were using all their skills and knowledge and carrying out a thorough search according to the methods, they had not used their brains.
+*A.Complex Sentence
+B.Compound Sentence
+C.Simple Sentence
+D.Compound-Complex Sentence
+
+75.I we had a boat, we ________ the river.
+A.will cross
+B.would be crossing
+C.would have crossed
+*D.would cross
+
+76.Father said to me, “May you pass the examination” 
+*A.Father prayed that I might pass the examination.
+B.Father said to me that I may pass the examination.
+C.Father prayed that I may pass the examination.
+D.Father said to me that I must pass the examination.
+
+77.I can do it in _______ hour and _______ half.
+A.a, an
+B.an, no article
+C.an, the
+*D.an, a
+
+78.What did you eat for _______ breakfast this morning?
+A.the
+B.a
+C.both a and b
+*D.none
+
+79.Find the correctly spelt word: Simaltanious, Simultaneous, Simultanious, Simaltaneous
+A.Simaltanious
+*B.Simultaneous
+C.Simultanious
+D.Simaltaneous
+
+80.You ___ this. Why did not you use a chance?
+A.can do
+*B.could have done
+C.must do
+D.may do
+
+81.Which country has recently introduced a new 10 years Blue Residency Visa for individuals who have made outstanding contributions to Environmental Protection?
+*A. UAE
+B. Kuwait
+C. Qatar
+D. Saudi Arabia
+
+82.India has won the T20 World Cup twice. Which other teams have also won the ICC Men’s T20 World Cup twice?
+A. Pakistan and South Africa
+*B. West Indies and England
+C. Australia and England
+D. West Indies and South Africa
+
+83.Which country launched its first space agency in June 2024 and plans to make a Mars landing by 2045?
+A. Japan
+*B. South Korea
+C. India
+D. China
+
+84.Which Country is second largest Contributor to UN after USA?
+A. Russia
+B. Saudi Arabia
+*C. China
+D. France
+
+85.Which Country has been Ranked as the Worlds Second Largest Weapons Manufacturer?
+A. USA
+B. Russia
+*C. China
+D. Pakistan
+
+86.Where did Pakistan rank on the GFP Index for global military power?
+A.7th
+B.9th
+*C.12th
+D.14th
+
+87.Which organization downgraded Pakistan's GDP growth outlook for 2025 to 3%?
+*A.IMF
+B.UN
+C.ADB
+D.World Bank
+
+88.Who has been appointed as the Inspector General (IG) of Khyber Pakhtunkhwa?
+A.Arshad Khan
+B.Aslam Khan
+*C.Zulfiqar Hameed
+D.Muhammad Rizwan
+
+89.According to the Henley Passport Index 2025, what is the global ranking of Pakistan's passport?
+A.102nd
+*B.103rd 
+C.104th
+D.None of these
+
+90.Who holds the Presidency of UNSC for January 2025?
+A.Russia
+B.US
+C.China
+*D.Algeria
+
+91.On January 1, 2025, which two countries became full members of Schengen Area?
+A.Croatia and Poland
+*B.Romania and Bulgaria
+C.Hungary and Slovakia
+D.Bulgaria and Cyprus
+
+92.Which country did the US remove from the U.S. terrorism list?
+A.Iran
+*B.Cuba
+C.Syria
+D.North Korea
+
+93.What is the ranking of the PAF in the world’s most powerful air forces by Global Firepower?
+A.5th 
+B.4th
+*C.7th
+D.2nd
+
+94. Who won the 3rd SAARC Snooker Championship?
+A.Muhammad Bilal
+*B.Mohammad Asif
+C.Amir Tariq
+D.None of these
+
+95.What new law did Pakistan’s National Assembly pass on January 21, 2025?
+A.Digital Media Regulation Act
+B.None of these
+C.Social Media Control Act
+*D.Social Media Protection and Regulation Law
+
+96.Who is the current NATO Chief?
+A.Anders Fogh Rasmussen
+*B.Mark Rutte
+C.Jens Stoltenberg
+D.Alessandro Minuto-Rizzo
+
+97.Which edition of General Election was held in Pakistan on 8 February 2024?
+A.10
+B.11
+*C.12
+D.13
+
+98.Which country hosted high-level meeting on Israel-Palestine two-state solution on 13 September 2024?
+A.Qatar
+*B.Spain
+C.UAE
+D.Lebanon
+
+99.Justice Yahya Afridi is the ______Chief Justice of Pakistan.
+A.29th
+*B.30th
+C.31st
+D.None of these
+
+100.Which country has recently become the world most sanctioned country in the World?
+A.Iran
+B.North Korea
+*C.Russia
+D.Venezuela
 
 
 
